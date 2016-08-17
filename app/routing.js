cffa18ee6b6e4cd353cb8a9ff3f618a2e4d181e2
Zabanaa <karim.cheurfi@gmail.com>
2016-08-17 14:16:37 +0000

    Add routing config

---
+++ app/routing.js
@@ -0,0 +1,9 @@
+export default routing = ($urlRouterProvider, $locationProvider) => {
+    
+    // TODO set html5 mode on
+
+    // ADD YOUR ROUTES HERE BRUV
+    $urlRouterProvider.otherwise("/")
+
+}
+
