8623de0bb6200dfda07bd4299c27b6e05555d426
Zabanaa <karim.cheurfi@gmail.com>
2016-08-17 14:17:05 +0000

    Initial app setup with config

--- app/app.js
+++ app/app.js
@@ -1,2 +1,10 @@
+import angular from 'angular'
+import uirouter from 'angular-ui-router'
+import routing from './routing'
+
+
+angular.module('app', [uirouter])
+    .config(routing)
+
 console.log("Hello Bro")  
 
