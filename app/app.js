8c3c3f17938dd19dd27bab989a072e04a407f185
Zabanaa <karim.cheurfi@gmail.com>
2016-08-20 14:40:18 +0000

    plug service to main module

--- app/app.js
+++ app/app.js
@@ -3,8 +3,10 @@ import uirouter from 'angular-ui-router'
 import routing from './routing'
 import HomeController from './controllers/home'
 import VenuesController from './controllers/venues'
+import VenuesService from './services/fsq'
 
 angular.module('app', [uirouter])
     .config(routing)
     .controller('HomeController', HomeController)
     .controller('VenuesController', VenuesController) 
+    .service('VenuesService', VenuesService)
