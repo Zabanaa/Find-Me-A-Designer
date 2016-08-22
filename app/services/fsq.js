86b1c8bd21ce7a4b166aa61d12061e06ccda4df7
Zabanaa <karim.cheurfi@gmail.com>
2016-08-22 09:45:14 +0000

    Add get and set venues

--- app/services/fsq.js
+++ app/services/fsq.js
@@ -6,10 +6,19 @@ export default class VenuesService {
         this.clientId = 'Y5IGDTNYOSPCOOXGSHBC0XGALFKBNO0MSZFA1QSXP3LKILG5'
         this.clientSecret = '3RCSCB0GEVZX3CERI2DC4I1LU5G41ZPXFB2BUFPHWHVWBJRB'
         this.version = "20160820"
+        this.venues = {}
     }
     
-    getVenues(params){
+    fetchVenues(params){
         return this.get(this.endpoint + 'near=' + params.location + '&query='+ params.query + '&price=' + params.price +'&limit=' + params.limit+ '&radius='+ params.radius +'&client_id=' + this.clientId + '&client_secret=' + this.clientSecret + '&v=' + this.version) 
+    }
+    
+    getVenues(){
+        return this.venues 
+    }
+
+    setVenues(data){
+        this.venues = data.response.groups['0'].items
     }
 
 }
