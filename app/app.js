a5a488fc327151819bb11ddd54aa389d586f2336
Zabanaa <karim.cheurfi@gmail.com>
2016-08-25 09:22:26 +0000

    Import maps service

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@ import routing from './routing'
 import HomeController from './controllers/home'
 import VenuesController from './controllers/venues'
 import VenuesService from './services/fsq'
+import MapsService from './services/maps'
 
 angular.module('app', [uirouter])
     .config(routing)
     .controller('HomeController', HomeController)
     .controller('VenuesController', VenuesController) 
     .service('VenuesService', VenuesService)
+    .service('MapsService', MapsService)
