bcb0075c82e135593795c7fefd5f599cfb015c71
Zabanaa <karim.cheurfi@gmail.com>
2016-08-26 09:34:33 +0000

    Create modal functionality

--- app/controllers/venues.js
+++ app/controllers/venues.js
@@ -4,10 +4,38 @@ export default class VenuesController {
         this.venues = VenuesService.getVenues()
         this.searchLocation = VenuesService.searchLocation
         this.userSearchQuery = VenuesService.userSearchQuery
+        this.modalShown = false
     }
 
     showDetail(venue){
         console.log(venue)
+    }
+
+    openModal(venue) {
+        this.modalShown = true 
+        let modalVenue = venue.venue
+        this.venue = {
+            name: modalVenue.name,
+            phone: modalVenue.contact.formattedPhone,
+            twitter: modalVenue.contact.twitter,
+            url: modalVenue.url, 
+            address: modalVenue.location.formattedAddress.join(),
+            rating: modalVenue.rating,
+            ratingColor: modalVenue.ratingColor,
+            isOpen: modalVenue.hours.isOpen,
+            hereNow: modalVenue.hereNow.count,
+            checkins: modalVenue.stats.checkinsCount,
+            tips: modalVenue.stats.tipCount,
+            latLng: {lat: modalVenue.location.lat, lng: modalVenue.location.lng}
+        } 
+
+        // Make call to the google maps api
+
+    }
+
+    closeModal() {
+        this.modalShown = false
+        this.venue = null
     }
 
 }
