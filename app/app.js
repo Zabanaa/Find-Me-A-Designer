1ac8159413ae73865a6c00cf9d3bfffdf0caeed1
Zabanaa <karim.cheurfi@gmail.com>
2016-08-30 07:25:33 +0000

    remove MapsService injection

--- app/app.js
+++ app/app.js
@@ -4,11 +4,9 @@ import routing from './routing'
 import HomeController from './controllers/home'
 import VenuesController from './controllers/venues'
 import VenuesService from './services/fsq'
-import MapsService from './services/maps'
 
 angular.module('app', [uirouter])
     .config(routing)
     .controller('HomeController', HomeController)
     .controller('VenuesController', VenuesController) 
     .service('VenuesService', VenuesService)
-    .service('MapsService', MapsService)
