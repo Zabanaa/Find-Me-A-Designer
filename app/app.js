97b2c6f5ce71fa185d4d466fbc799f41a89750c2
Zabanaa <karim.cheurfi@gmail.com>
2016-09-05 09:21:14 +0000

    Add ngAnimate module

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 import angular from 'angular'
 import uirouter from 'angular-ui-router'
+import angularanimate from 'angular-animate' 
 import routing from './routing'
 import HomeController from './controllers/home'
 import VenuesController from './controllers/venues'
 import VenuesService from './services/fsq'
 
-angular.module('app', [uirouter])
+angular.module('app', [uirouter, angularanimate])
     .config(routing)
     .controller('HomeController', HomeController)
     .controller('VenuesController', VenuesController) 
